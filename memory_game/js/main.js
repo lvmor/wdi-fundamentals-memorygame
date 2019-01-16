c6575b39b47ee196a4f989c10239c9cb15a6b9dd
Dr3i <dr3i@Dr3is-MBP.hsd1.ca.comcast.net>
2019-01-16 04:37:08 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,25 @@
-console.log("up and running");
+var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardsInPlay = [];
 
-console.log('user flipped ' + cardOne);
+var cardOne = cards[0];
+
+cardsInPlay.push('cardOne');
+
+
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[1];
+
+cardsInPlay.push('cardTwo');
+
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
